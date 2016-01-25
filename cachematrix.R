7298d99cadfa39051341bfeada2e1ac3131f1a0c
karthickgopalswamy <karthickgopalswamy@gmail.com>
2016-01-24 23:22:09 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,20 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## makeChacheMatrix function creates the matrix that can be given to cacheSolve
 makeCacheMatrix <- function(x = matrix()) {
  m <- NULL
+ ## function to set the data inputed to the makeChachematrix function
   set <- function(y) {
-    x <<- y
+    x <<- y 
     m <<- NULL
+    ## m is the inverse of the matrix and we assign it to NULL intitially
   }
+  ## function to get the data inputed to the makeChachematrix function
   get <- function() x
+  ## function to set inverse of the matrix
   setinverse <- function(inverse) m <<- inverse
+  ## function to getinverse 
   getinverse <- function() m
   list(set = set, get = get,
        setinverse = setinverse,
@@ -20,6 +25,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## cacheSolve will take the matrix created above using makeCacheMatrix function
+## and if it has inverse it will return the cahcehd data else it will update 
+## the inverse of the matrix using setinverse function of the object
 
 cacheSolve <- function(x, ...) {
 
